825ef7e72658dd0b3bbe66f17adb6918261db2a3
Commit-Commander <kartikeynarayan9598@gmail.com>
2025-02-27 17:22:19 +0530

    Adding changes in frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
   return (
     <div>
       {/* Heading of the app */}
-      <h1>To-Do List</h1>
+      <h1>To-Do List - Jenkins CI-CD Pipeline 🚀</h1>
       
       {/* Input field for the new task */}
       <input
